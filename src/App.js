386b945cf86956a39625632b5a830c262f9be910
Kaleeva  Alina <69644331+KaleevaAlina@users.noreply.github.com>
2021-09-24 08:25:51 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import DopContainer from "./default module/components/DopContainer";
+
+function App() {
+  return <DopContainer />;
+}
+
+export default App;
---
+++ src/Appn.js
@@ -0,0 +1,10 @@
+import './App.css';
+import DopContainern from './default module/components/DopContainern';
+
+function Appn() {
+  return (  
+      <DopContainern/>
+  );
+}
+
+export default Appn;
